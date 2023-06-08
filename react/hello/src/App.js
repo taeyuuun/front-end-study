232e238f0ac4f36adaf8ebc8488981466fbe2f03
taeyoooon <leteyune101010@gmail.com>
2023-06-09 08:49:49 +0900

    블로그 좋아요 기능 추가

--- react/hello/src/App.js
+++ react/hello/src/App.js
@@ -4,15 +4,30 @@ import './App.css';
 function App() {
 
   let post = '강남 우동 맛집';
-  useState
+  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
     return(
       <div className="App">
         <div className="black-nav">
-          <h4>블로그</h4>
+          <h4>ReactBlog</h4>
         </div>
+
+        <button onClick={ b[0]('여자코트 추천') }>여자코트 추천</button>
+
         <div className="list">
-          <h4>글제목</h4>
+          <h4>{ 글제목[0] } <span onClick={ 따봉변경(따봉 + 1) }>👍</span> {따봉} </h4>
+          <p>2월 17일 발행</p>
+        </div>  
+
+
+
+        <div className="list">
+          <h4>{ 글제목[1] }</h4>
+          <p>2월 17일 발행</p>
+        </div>  
+        <div className="list">
+          <h4>{ 글제목[2] }</h4>
           <p>2월 17일 발행</p>
         </div>  
       </div>
